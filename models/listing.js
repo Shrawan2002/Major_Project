61020ea83c959feb2ad21bafb63a6cde6de44097
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-10 20:48:59 +0530

    basic setup linting route

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title :{
+     types: String,
+     required: true
+    },
+    description : String,
+    image: {
+        types: String,
+        set: ((v)=> v === ""? 
+        "https://www.istockphoto.com/photo/palm-tree-on-the-beach-gm638202904-114290537" : v)
+    },
+    price: String,
+    location: String,
+    country: String,
+})
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
