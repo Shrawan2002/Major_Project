c3ae12e28a81ef94fd3d15cc86e8a5286164b32d
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-11 09:10:23 +0530

    add init file

--- models/listing.js
+++ models/listing.js
@@ -3,14 +3,14 @@ const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
     title :{
-     types: String,
+     type: String,
      required: true
     },
     description : String,
     image: {
-        types: String,
-        set: ((v)=> v === ""? 
-        "https://www.istockphoto.com/photo/palm-tree-on-the-beach-gm638202904-114290537" : v)
+        type: String,
+        default: "https://www.istockphoto.com/photo/palm-tree-on-the-beach-gm638202904-114290537",
+        set: ((v)=> v === ""?"https://www.istockphoto.com/photo/palm-tree-on-the-beach-gm638202904-114290537" : v)
     },
     price: String,
     location: String,
