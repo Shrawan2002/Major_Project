c1eb7b829038fe0935182f34fc921114298fc81b
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-31 21:19:33 +0530

    connect-flash

--- classroom/server.js
+++ classroom/server.js
@@ -3,6 +3,8 @@ const app = express();
 // const cookiesParser = require("cookie-parser");
 
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
 
 
 
@@ -51,6 +53,8 @@ const session = require("express-session");
 //     res.send(`You sent a request ${req.session.count} times`);
 // })
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const sessionOptions = {
     secret: "mysupersecretstring",
@@ -59,17 +63,39 @@ const sessionOptions = {
 }
 
 app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req,res,next)=>{
+   res.locals.successMsg = req.flash("success");
+   res.locals.errorMsg = req.flash("error");
+   next();
+})
 
 app.get("/register", (req,res)=>{
     let {name = "anonymous"} = req.query;
     req.session.name = name;
-    console.log(req.session.name); 
+    // console.log(req.session.name); 
     // res.send(name);
+    if(name === "anonymous"){
+        req.flash("error", "user not registered")
+    }else{
+        req.flash("success", "user registered successfully!");
+    }
     res.redirect("/hello");
 })
 
+// connect-flash works like a queue:
+// Don’t call req.flash("success") twice. Just assign it once:
 app.get("/hello", (req,res)=>{
-    res.send(`hello, ${req.session.name}`);
+    // res.send(`hello, ${req.session.name}`);
+    // console.log(req.flash("success"));
+    // let msg = req.flash("success");
+    //  let msg = req.flash("success")[0];
+    //  console.log(msg);
+
+    // res.locals.successMsg = req.flash("success");
+    // res.locals.errorMsg = req.flash("error");
+    res.render( "page.ejs", {name: req.session.name});
 })
 
 
