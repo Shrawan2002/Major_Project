163c835cf1e9dc340f2621c8d40fa58262a7f71c
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-31 12:49:30 +0530

    express - session

--- classroom/server.js
+++ classroom/server.js
@@ -1,31 +1,70 @@
 const express = require("express");
 const app = express();
-const cookiesParser = require("cookie-parser");
+// const cookiesParser = require("cookie-parser");
+
+const session = require("express-session");
 
 
 
-app.use(cookiesParser());
+// app.use(cookiesParser());
 
-app.get("/getcookies", (req,res)=>{
-    res.cookie("greet", "hello"); 
-    res.send("sent you some cookies!");
-})
+//    app.use(session({
+//     secret: "mysupersecretstring",
+//     resave: false,
+//     saveUninitialized: true
+// }));
+
+// app.get("/getcookies", (req,res)=>{
+//     res.cookie("greet", "hello"); 
+//     res.send("sent you some cookies!");
+// })
 
 //pars cookies
 
-app.get("/",(req,res)=>{
-    console.dir(req.cookies);
-    res.send("Hi, i am root");
-})
+// app.get("/",(req,res)=>{
+//     console.dir(req.cookies);
+//     res.send("Hi, i am root");
+// })
 
-app.get("/greet", (req,res)=>{
-    let {name = "anonymous"} = req.cookies;
-    res.send(`Hi, ${name}`)
-})
+// app.get("/greet", (req,res)=>{
+//     let {name = "anonymous"} = req.cookies;
+//     res.send(`Hi, ${name}`)
+// })
 
 // app.get("/", (req,res)=>{
 //     res.send("Hi, i am root");  
 // })
+
+
+//express session
+
+// app.get("/test", (req,res)=>{
+//     res.send("test successful");
+// })
+
+// app.get("/reqcount", (req,res)=>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }else{
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent a request ${req.session.count} times`);
+// })
+
+
+const sessionOptions = {
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true
+}
+
+app.use(session(sessionOptions));
+
+app.get("/register", (req,res)=>{
+    let {name} = req.query;
+    res.send(name);
+})
+
 
 app.listen(3000,()=>{
     console.log("server is listings on port",3000);
