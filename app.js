d8db6344260cde6788273c1849206261fa079eab
Shrawan2002 <shrawankumar1754@gmail.com>
2025-09-02 17:25:52 +0530

    add signup page

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate"); 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
 const flash = require("connect-flash");
@@ -63,21 +64,22 @@ app.use((req,res,next)=>{
     res.locals.error = req.flash("error");
     next();
 }) 
+ 
+
+// app.get("/demouser", async (req,res)=>{
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student"
+//     });
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// })
 
 
-app.get("/demouser", async (req,res)=>{
-    let fakeUser = new User({
-        email: "student@gmail.com",
-        username: "delta-student"
-    });
-    let registeredUser = await User.register(fakeUser, "helloworld");
-    res.send(registeredUser);
-})
 
-
-
-app.use("/listings", listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/", userRouter);
 
 
 
