fedc2bc6ff6375e6190a075de04c2a5aad50a197
Shrawan2002 <shrawankumar1754@gmail.com>
2025-09-15 11:22:39 +0530

    maxbox sdk

---
+++ public/js/map.js
@@ -0,0 +1,7 @@
+
+    mapboxgl.accessToken = mapToken;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        center: [ 77.2090, 28.6139], // starting position [Longitude, Latitude]. Note that lat must be set between -90 and 90
+        zoom: 9 // starting zoom
+    });
